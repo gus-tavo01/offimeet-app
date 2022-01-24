82426f7e000806a3eb8e7f9878fee00bf887178f
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-01-24 15:01:33 -0800

    add initial store

---
+++ src/state/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// TODO
+// import counterReducer from '../features/counter/counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: null, // TODO
+  },
+});
